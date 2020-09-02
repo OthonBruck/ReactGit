191e214ef16831db545703c3bdc83a874b490754
OthonBruck <othon.bruck@hotmail.com>
2020-09-02 09:44:21 -0300

    feat: Incialização do projeto react

---
+++ reactgit/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Lista from "./components/lista";
+
+function App() {
+  return (
+    <div className="App">
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactgit/src/components/lista.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class lista extends Component {
+  render() {
+    return (
+      <div>
+        <h1>REACT GIT</h1>
+      </div>
+    );
+  }
+}
